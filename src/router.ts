25aba4653b991d05699c08f26207f9806f59db09
viperxc24odmaker <viperxc2204@gmail.com>
2026-08-13 19:01:14 +0100

    chore: remove stale router scaffold

--- src/router.ts
+++
@@ -1,5 +0,0 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
-import DashboardView from './views/DashboardView.vue';
-import CreateServerView from './views/CreateServerView.vue';
-import ServerView from './views/ServerView.vue';
-export const router = createRouter({ history: createWebHashHistory(), routes: [{ path: '/', component: DashboardView }, { path: '/create', component: CreateServerView }, { path: '/server/:id', component: ServerView }] });
